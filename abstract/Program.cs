5d2ba36f73719ffe6f1d5803d15a6b9dcde9867a
Carlton Akussah <carlton@Carltons-MacBook-Pro.local>
2025-08-04 22:47:59 +0000

    third commit

---
+++ abstract/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle();
+            Shape rectangle = new Rectangle();
+
+            Console.WriteLine($"Area of the circle: {circle.GetArea()}");
+            Console.WriteLine($"Area of the rectangle: {rectangle.GetArea()}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        public override double GetArea()
+        {
+            const double pi = 3.142;
+            double radius = 9;
+
+            double result = pi * radius * radius;
+            return result;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override double GetArea()
+        {
+            double length = 8;
+            double width = 5;
+
+            double result = length * width;
+            return result;
+        }
+    }
+}
